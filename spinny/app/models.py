089209f5ce561b215c7f2a51384eaf04484a5701
Prashant <pra4093@gmail.com>
2024-01-07 18:11:02 +0530

    updated models

--- spinny/app/models.py
+++ spinny/app/models.py
@@ -1,12 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.db import models
 
 class Box(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE,editable=False)
     length = models.FloatField()
-    breadth = models.FloatField()
+    width = models.FloatField()
     height = models.FloatField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True,editable=False)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.creator + self.created_at + self.updated_at 
